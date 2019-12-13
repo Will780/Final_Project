601b68540a14ac60e9e5b5ca57c3d0fc3b370a68
Will North <will.north@comcast.net>
2019-12-13 09:33:10 -0700

    Begin canvas coding

--- Final.js
+++ Final.js
@@ -3,7 +3,7 @@ function ready() {
 
     angle.oninput = function() {
         resulta.innerHTML = angle.value;
-        var deg = angle.value
+        let deg = angle.value;
         document.getElementById('cannon').style = `transform:rotate(-${deg}deg);`;
       };
 
@@ -11,12 +11,47 @@ function ready() {
         resultb.innerHTML = power.value;
       };
 
-    height.oninput = function() {
-        resultc.innerHTML = height.value;
-      };
+    // height.oninput = function() {
+    //     resultc.innerHTML = height.value;
+    //   };
 
-    button.onclick = function draw() {
-      
-    }
+      let Canvas = document.getElementById("canvas");
+      let Ctx = null;
+      let Width = Canvas.width
+      let Height = Canvas.height
+      function MaxX() {
+        return 10;
+      }
+      function MinX() {
+        return -10;
+      }
+      function MaxY() {
+        return MaxX() * Height / Width;
+      }
+      function MinY() {
+        return MinX() * Height / Width;
+      }
+      function XC(x) {
+        return (x - MinX()) / (MaxX() - MinX()) * Width;
+      }
+      function YC(y) {
+        return Height - (y - MinY()) / (MaxY() - MinY()) * Height;
+      }
+
+      function Draw() {
+        Ctx = Canvas.getContext('2d');
+        Ctx.clearRect(0, 0, Width, Height);
+
+        Draw:
+        RenderFunction();
+      }
+      let XSTEP = (MaxX() - MinX()) / Width;
+
+      function RenderFunction() {
+        let first = true
+      }
+
+      Ctx.beginPath();
+      for (x = MinX()
 }
 document.addEventListener("DOMContentLoaded", ready);
