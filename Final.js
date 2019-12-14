35e2811c4e266a30feca4ba4ec0acc2af5355eba
Will North <will.north@comcast.net>
2019-12-14 15:27:59 -0700

    Arc working

--- Final.js
+++ Final.js
@@ -1,21 +1,25 @@
 function ready() {
     console.log("Page ready");
-
+  
+    // First slider
     angle.oninput = function() {
+        console.log("Input Changed")
         resulta.innerHTML = angle.value;
-        let deg = angle.value;
+        deg = angle.value;
+        deg1=angle.value;
         document.getElementById('cannon').style = `transform:rotate(-${deg}deg);`;
       };
-
+    // Second slider
     power.oninput = function() {
-        resultb.innerHTML = power.value;
+      resultb.innerHTML = power.value;
+      vel = power.value;
+      console.log("Vel =" + vel);
       };
 
-    // height.oninput = function() {
-    //     resultc.innerHTML = height.value;
-    //   };
-
-      let Canvas = document.getElementById("canvas");
+      angle.onchange = function() {
+        console.log("om cha" + deg);
+        let Canvas = document.getElementById("mycanvas");
+      console.log("Got canvas");
       let Ctx = null;
       let Width = Canvas.width
       let Height = Canvas.height
@@ -38,20 +42,41 @@ function ready() {
         return Height - (y - MinY()) / (MaxY() - MinY()) * Height;
       }
 
-      function Draw() {
+      //eval(document.getElementById().value);
+      let first = false
+      if (Canvas.getContext) {
         Ctx = Canvas.getContext('2d');
         Ctx.clearRect(0, 0, Width, Height);
-
-        Draw:
-        RenderFunction();
+        first = true;
       }
+
       let XSTEP = (MaxX() - MinX()) / Width;
-
-      function RenderFunction() {
-        let first = true
+      
+      let vel = 1
+      console.log("Velocity =" + vel);
+      console.log("Deg =" + deg);
+      
+      Ctx.beginPath();
+      for (let x = MinX(); x <= MaxX(); x += XSTEP) {
+        let y = x * (vel * Math.sin(deg)) / (vel * Math.cos(deg)) - 9.8 * (x / vel * Math.cos(deg)) * (x / vel * Math.cos(deg)) / 2;
+        if (first) {
+          Ctx.moveTo(XC(x),YC(y));
+          first = false
+        } else {
+          Ctx.lineTo(XC(x),YC(y));
+        }
+      }
+      Ctx.stroke();
       }
 
-      Ctx.beginPath();
-      for (x = MinX()
+    // Third slider?
+    // height.oninput = function() {
+    //     resultc.innerHTML = height.value;
+    //   };
+    // Canvas setup
+
+  
+
+      
 }
 document.addEventListener("DOMContentLoaded", ready);
